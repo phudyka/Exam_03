f2388e3da6c6a2a4381ef5a8f49c3b8538bf1954
Paul Hudyka <phudyka@c3r1p2.42nice.fr>
2023-04-06 15:22:57 +0200

    ok ft_printf easy to remember

---
+++ ft_printf/ft_printf.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int ft_printstr(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return(write(1, "(null)", 6));
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	return (i);
+}
+
+int	ft_printnbr(long long int nbr, int base)
+{
+	int		len = 0;
+	char	*hex = "0123456789abcdef";
+
+	if (nbr < 0)
+	{
+		nbr *= -1;
+		len += write(1, "-", 1);
+	}
+	if (nbr >= base)
+		len += ft_printnbr((nbr / base), base);
+	len += write(1, &hex[nbr % base], 1);
+	return (len);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	int i = 0;
+	int len = 0;
+	va_list	arg;
+	
+	va_start(arg, format);
+	while (format[i])
+	{
+		if ((format[i] == '%') && (format[i + 1] != '\0'))
+		{
+			i++;
+			if (format[i] == 's')
+				len += ft_printstr(va_arg(arg, char *));
+			else if (format[i] == 'd')
+				len += ft_printnbr((long long int)va_arg(arg, int), 10);
+			else if (format[i] == 'x')
+				len += ft_printnbr((long long int)va_arg(arg, unsigned int), 16);
+		}
+		else
+			len += write(1, &format[i], 1);
+		i++;
+	}
+	va_end(arg);
+	return (len);
+}
