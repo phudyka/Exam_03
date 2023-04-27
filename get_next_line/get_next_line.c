49fab161724a8dfeb6e039c837d63748147d0bb1
Paul Hudyka <phudyka@c1r8p1.42nice.fr>
2023-04-27 09:26:38 +0200

    ok...?

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,135 +1,151 @@
 #include "get_next_line.h"
 
-int ft_strlen(char *str)
+size_t  ft_strlen(const char *str)
 {
-	int i = 0;
+    size_t  i = 0;
 
-	while (str[i])
-		i++;
-	return (i);
+    if (!str)
+        return (0);
+    while (str[i])
+        i++;
+    return (i);
 }
 
-char *ft_strcpy(char *s1, char *s2)
+int ft_slasher(const char *s, int c)
 {
-	int i = 0;
+    int	i = 0;
 
-	while (s2[i])
-	{
-		s1[i] = s2[i];
-		i++;
-	}
-	s1[i] = '\0';
-	return(s1);
-}
-
-char *ft_strncpy(char *s1, char *s2, int n)
-{
-    int i = 0;
-
-    while (s2[i] && i < n)
+    if (!s)
+        return (0);
+    while (s[i])
     {
-        s1[i] = s2[i];
+        if (s[i] == (char)c)
+            return (1);
         i++;
     }
-    s1[i] = '\0';
-    return(s1);
+    return (0);
 }
 
-char *ft_strchr(char *s, int c)
+char    *ft_strjoin(char *s1, char *s2)
 {
-	int i = 0;
+    size_t  i;
+    size_t  j;
+    char    *join;
 
-	while (s[i])
-	{
-		if (s[i] == (char)c)
-			return(s + i);
-		i++;
-	}
-	return(NULL);
-}
-
-char *ft_strjoin(char *s1, char *s2)
-{
-	char *joined;
-	int i = -1;
-	int j = -1;
-	int len1 = 0;
-	int len2 = 0;
-    
-    if (s1)
-        len1 = ft_strlen(s1);
-    if (s2)
-        len2 = ft_strlen(s2);
-    if(!(joined = (char *)malloc(sizeof(char) * (len1 + len2 + 1))))
+    if (!s1)
+    {
+        s1 = (char *)malloc(sizeof(char) * 1);
+        *s1 = '\0';
+    }
+    if (!s1 || !s2)
         return (NULL);
-    while (++i < len1)
-        joined[i] = s1[i];
-    while (++j < len2)
-        joined[i + j] = s2[j];
-    joined[i + j] = '\0';
-    free(s1);
-    return (joined);
+    if (!(join = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1))))
+        return (NULL);
+    i = -1;
+    while (s1[++i])
+        join[i] = s1[i];
+    j = -1;
+    while (s2[++j])
+        join[i + j] = s2[j];
+    join[i + j] = '\0';
+    free (s1);
+    return (join);
 }
 
-char *ft_cut(char *temp)
+char    *ft_cut(char *temp)
 {
-    char *cut;
-    int i = 0;
+    size_t  i = 0;
+    size_t  j = 0;
+    char    *cut;
+
+    if (!*temp)
+        return (NULL);
     while (temp[i] && temp[i] != '\n')
         i++;
-    cut = (char *)malloc(i + 2);
+    cut = (char *)malloc(sizeof(char) * (i + 2));
     if (!cut)
-        return NULL;
-    ft_strncpy(cut, temp, i);
-    cut[i] = '\n';
-    cut[i + 1] = '\0';
-    return cut;
-}
-
-char *ft_recut(char *temp)
-{
-    char *recut;
-    char *pos = ft_strchr(temp, '\n');
-    if (!pos)
+        return (NULL);
+    while (temp[j] && j <= i)
     {
-        free(temp);
-        return NULL;
+        cut[j] = temp[j];
+        j++;
     }
-    size_t len = ft_strlen(pos + 1);
-    recut = (char *)malloc(len + 1);
-    if (!recut)
-        return NULL;
-    ft_strcpy(recut, pos + 1);
-    free(temp);
-    return recut;
+    cut[j] = '\0';
+    return (cut);
 }
 
-char *ft_read(int fd, char *temp)
+char    *ft_recut(char  *temp)
 {
-    char buf[BUFFER_SIZE + 1];
-    int byte = 1;
-    while (!ft_strchr(temp, '\n') && byte > 0)
+    size_t  i;
+    size_t  j;
+    char    *recut;
+
+    i = 0;
+    while (temp[i] && temp[i] != '\n')
+        i++;
+    if (!temp[i])
     {
-        byte = read(fd, buf, BUFFER_SIZE);
+        free (temp);
+        return (NULL);
+    }
+    recut = (char *)malloc(sizeof(char) * (ft_strlen(temp) -i + 1))
+        return (NULL);
+    j = 0;
+    while (temp[i++])
+        recut[j++] = (temp[i]);
+    recut[j] = '\0';
+    free(temp);
+    return (recut);
+}
+
+char    *ft_read(int fd, char *temp)
+{
+    int     byte = 1;
+    char    mini[BUFFER_SIZE + 1];
+
+    while (!ft_slasher(temp, '\n') && byte)
+    {
+        byte = read(fd, mini, BUFFER_SIZE);
         if (byte == -1)
+        {
+            free (mini);
             return (NULL);
-        buf[byte] = '\0';
-        temp = ft_strjoin(temp, buf);
+        }
+        mini[byte] = '\0';
+        temp = ft_strjoin(temp, mini);
     }
     return (temp);
 }
 
-char *get_next_line(int fd)
+char    *get_next_line(int fd)
 {
-	char *line;
-	static char *temp;
+    char        *line;
+    static char *maxi;
 
-	if (!fd || BUFFER_SIZE <= 0 || read(fd, &temp, 0) < 0)
-		return(NULL);
-	temp = ft_read(fd, temp);
-	if (!temp)
-		return (NULL);
-	line = ft_cut(temp);
-	temp = ft_recut(temp);
-	return(line);
+    if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &maxi, 0) < 0)
+        return (NULL);
+    maxi = ft_read(fd, maxi);
+    if (!maxi)
+        return (NULL);
+    line = ft_cut(maxi);
+    maxi = ft_recut(maxi);
+    return (line);
 }
+
+/*
+int main(void)
+{
+    char    *str;
+    int fd = open("test.txt", O_RDONLY);
+
+    int i = 100;
+    while (i--)
+    {
+        str = get_next_line(fd);
+        if (!str)
+            break;
+        printf("STR :%s", str);
+        free (str);
+    }
+    system ("leaks a.out");
+}*/
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -4,12 +4,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #ifndef BUFFER_SIZE
 #define BUFFER_SIZE 5
 #endif
 
+size_t  ft_strlen(const char *str);
+int ft_slasher(const char *s, int c);
+char    *ft_strjoin(char *s1, char *s2);
+char    *ft_cut(char  *temp)
+char    *ft_recut(char  *temp);
+char    *ft_read(int fd, char *temp);
 char *get_next_line(int fd);
 
 #endif
